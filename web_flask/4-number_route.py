8c1ecdd0852f3940c1848ac75bf8c94a25dba1e3
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2024-02-16 06:31:55 -0800

    Task4. Is it a number?

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+''' Script that starts a Flask web application '''
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """ hello home page """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ HBNB page """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """ Display 'C ' followed by the value of the text variable
+    (replace underscore _ symbols with a space  """
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text='is cool'):
+    """ Display 'Python ', followed by the value of the text variable
+    (replace underscore _ symbols with a space ) """
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+@app.route("/number/<int:num>", strict_slashes=False)
+def number(num):
+    """ Display 'n is a number' only if n is an integer """
+    return "{:d} is a number".format(num)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=False)
