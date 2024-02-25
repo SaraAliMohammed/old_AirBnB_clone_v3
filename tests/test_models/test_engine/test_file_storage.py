a5de3efaeeb3cf5b568fdcf785cf77d70a91277b
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2024-02-25 00:40:02 -0800

    Fix pycodestyle error

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -8,6 +8,7 @@ from models.base_model import BaseModel
 from models.user import User
 from models.engine.file_storage import FileStorage
 
+
 @unittest.skipIf(
     os.getenv('HBNB_TYPE_STORAGE') == 'db', 'FileStorage test')
 class TestFileStorage(unittest.TestCase):
